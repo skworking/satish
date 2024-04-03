bb37847c4a122e337113f319ab93015a7adcd73f
satish-kumar <satishgupta9792070@gmail.com>
2024-04-03 11:30:53 +0530

    add on logout navigate to login page

--- src/app/component/protected/protectedRoute.js
+++ src/app/component/protected/protectedRoute.js
@@ -1,16 +1,28 @@
 'use client'
 import { useRouter } from 'next/navigation'
 // import { redirect } from 'next/navigation'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const ProtectedRoute = ({children}) => {
     const router=useRouter();
+    const [token, setToken] = useState( sessionStorage.getItem('jwt') );
     useEffect(()=>{
-    const token = sessionStorage.getItem('jwt');
+    // const token = sessionStorage.getItem('jwt');
     if (!token) {
-        // redirect('/login')
+      // redirect('/login')
       router.push('/login');
+      // const handleStorageChange = () => {
+      //     setAuth(!!sessionStorage.getItem('jwt'));
+      // };
+      // window.addEventListener('storage', handleStorageChange);
+  
+      // // Clean up listener
+      // return () => {
+      //   window.removeEventListener('storage', handleStorageChange);
+      // };
+     
     }
+    
     },[])
   return <>{children}</>
   
--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,59 +1,50 @@
 'use client'
-
+import React from 'react';
 import Link from 'next/link';
-import React, { useState } from 'react'
-import { BsBrilliance } from "react-icons/bs";
+import { BsBrilliance } from 'react-icons/bs';
 
-const NavList = () => {
-    const [isAuth, setAuth] = useState(typeof window !== 'undefined' ? sessionStorage.getItem('jwt') : null);
-    // const [isAuth, setAuth] = useState( sessionStorage.getItem('jwt') );
-    return (
-        <>
-            <ul className='flex gap-2' >
-                {isAuth ? (<>
-                    <li className='hover:bg-white p-2 rounded'>
-                        <Link href={'/add'} className='p-2'>Add USer</Link>
-                    </li>
-                    <li className='hover:bg-white p-2 rounded'>
-                        <Link href={'/user-list'} className='p-2'>
-                            Display USer
-                        </Link>
-                    </li>
-                    {/* <li className='hover:bg-white p-2 rounded'>
-                        <Link href={'/excel-list'} className='p-2'>
-                            Exel-CSV
-                        </Link>
-                    </li> */}
-                    <li className='hover:bg-white p-2 rounded'>
-                        <Link href={'/import-file'} className='p-2'>
-                            Upload EXCEL
-                        </Link>
-                    </li>
-                </>) :
-                    (
-                        <>
-                            <li className='hover:bg-white p-2 rounded'>
-                                <Link href={'/login'} className='p-2'>
-                                    Login
-                                </Link>
-                            </li>
-                            
-                        </>
-                    )}
+const NavList = ({ isAuth ,onlogout}) => {
+    const handleLogout = () => {
+        sessionStorage.removeItem('jwt');
+        onlogout()
+    };
 
-            </ul>
-        </>
-    )
-}
-const Navbar = () => {
     return (
-        <div className='flex w-full  justify-around h-[50px] py-2 items-center bg-slate-500'>
+        <ul className='flex gap-2'>
+            {isAuth ? (
+                <>
+                    <li className='hover:bg-white p-2 rounded'>
+                        <Link href='/add'>Add User</Link>
+                    </li>
+                    <li className='hover:bg-white p-2 rounded'>
+                        <Link href='/user-list'>Display User</Link>
+                    </li>
+                    <li className='hover:bg-white p-2 rounded'>
+                        <Link href='/import-file'>Upload EXCEL</Link>
+                    </li>
+                    <li className='hover:bg-white p-2 rounded' onClick={handleLogout}>
+                        <Link href='/login' className='p-2'>Logout</Link>
+                    </li>
+                </>
+            ) : (
+                <li className='hover:bg-white p-2 rounded'>
+                    <Link href='/login' className='p-2'>Login</Link>
+                </li>
+            )}
+        </ul>
+    );
+};
+
+const Navbar = ({isAuth,onlogout}) => {
+  
+    return (
+        <div className='flex w-full justify-around h-[50px] py-2 items-center bg-slate-500'>
             <Link href='/'>
                 <BsBrilliance className='fill-red-200 w-[60px] h-[40px]' />
             </Link>
-            <NavList />
+            <NavList isAuth={isAuth} onlogout={onlogout}/>
         </div>
-    )
-}
+    );
+};
 
 export default Navbar;
